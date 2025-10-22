df3d291dcb7f00d350fe2da18dcec7b511476011
Ahmedmeid99 <ahmedalbakrycool@gmail.com>
2025-10-23 00:24:07 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // 👈 enables static export (replaces `next export`)
+  images: {
+    unoptimized: true, // required if you use <Image /> in static export
+  },
 };
 
 export default nextConfig;
